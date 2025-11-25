c5ee89d4384282ed2eaaae09aaf8cdd012c259f1
Jonathan Rogenes <jonathanrogenes@gmail.com>
2025-11-25 17:36:05 +0900

    nothing

--- app/controllers/fight_questions_controller.rb
+++ app/controllers/fight_questions_controller.rb
@@ -1,2 +1,15 @@
 class FightQuestionsController < ApplicationController
+
+  def index
+    @fight = Fight.find(params[:id])
+    @fight_questions = @fight.fight_questions
+  end
+
+
+  private
+
+  def question_params
+    params.require(:question).permit(:difficulty, :anwers)
+  end
+
 end
