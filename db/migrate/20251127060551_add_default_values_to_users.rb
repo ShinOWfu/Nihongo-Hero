bd0771cb33d358858d23aa976b1d30e89613ca85
Haley Banfield <HaleyKBanfield@gmail.com>
2025-11-28 11:47:28 +0900

    I believe I've remedied the issue. Please check.

---
+++ db/migrate/20251127060551_add_default_values_to_users.rb
@@ -0,0 +1,12 @@
+class AddDefaultValuesToUsers < ActiveRecord::Migration[7.1]
+  def change
+    change_column_default :users, :experience_points, from: nil, to: 0
+    change_column_default :users, :level, from: nil, to: 1
+    change_column_default :users, :hitpoints, from: nil, to: 100
+
+    # Update existing users with nil values
+    User.where(experience_points: nil).update_all(experience_points: 0)
+    User.where(level: nil).update_all(level: 1)
+    User.where(hitpoints: nil).update_all(hitpoints: 100)
+  end
+end
