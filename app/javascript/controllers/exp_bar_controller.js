a7ec57a3c635ca6e3088bb9bad7bcf0cc34d3030
Oliver Will <o.will@alumni.maastrichtuniversity.nl>
2025-12-02 10:37:44 +0900

    Animation EXP and LEVEL

--- app/javascript/controllers/exp_bar_controller.js
+++ app/javascript/controllers/exp_bar_controller.js
@@ -3,7 +3,18 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     static values = {newExp: Number, oldExp: Number, levelUp: Boolean}
   connect() {
-    console.log(this.element, this.newExpValue)
-    this.element.style.width = `${this.newExpValue}%`
+  if (levelUp) {
+    // --- First fill up to 100% to show level up ---
+    this.element.style.width = '100%';
+    // await new Promise(resolve => setTimeout(resolve, 300)); 
+
+    // --- Then fill it to the new experience value percentage ---
+    this.element.style.width = `${this.newExpValue}%`;
+
+  } else {
+    // --- Level Up is not true (Standard experience gain) ---
+    this.element.style.width = `${this.newExpValue}%`;
+  }
   }
 }
+
