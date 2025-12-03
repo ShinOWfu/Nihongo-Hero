b75dd0b0064c792ad0016a45226e688dc2cb22de
Leandro Trabucco <leandrotrabucco@hotmail.com>
2025-12-03 17:51:53 +0900

    commented line on users controller that breaks everything

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,26 +2,26 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     # Calculate how many levels the user has completed
-    @levels_cleared = @user.fights.where(status: "completed").last.pluck(:story_level_id)
+    # @levels_cleared = @user.fights.where(status: "completed").last.pluck(:story_level_id)
     # Calculate the average percentage of correct answers
     all_count = @user.fight_questions.all.count
     correct_count = @user.fight_questions.joins(:question).where('fight_questions.selected_index = questions.correct_index').count
     @avg_perc_correct = correct_count.to_f/all_count * 100
-    # Calculate the streak of how many days in a row the user has completed at least one level 
+    # Calculate the streak of how many days in a row the user has completed at least one level
     @days_streak = streak
   end
 
   def streak
-    unique_creation_dates = @user.fight_questions.pluck(:created_at).map(&:to_date).uniq.sort                                     
+    unique_creation_dates = @user.fight_questions.pluck(:created_at).map(&:to_date).uniq.sort
     # If there are no questions, the streak is 0
     return 0 if unique_creation_dates.empty?
-      
+
     last_question_date = unique_creation_dates.last
       #If our last correct question was today or yesterday, we start the streak at 1
-    
+
     # If last question is before yesterday, there is no streak
     if last_question_date < Date.yesterday
-      return 0 
+      return 0
     end
 
     # Otherwise, we initialize our streak at 1 day
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
     (unique_creation_dates.length - 2).downto(0) do |i|
     current_date = unique_creation_dates[i]
     previous_date = unique_creation_dates[i+1]
-    
+
     # Check if the current date is exactly one day before the next date (consecutive)
       if current_date == previous_date - 1.day
         current_streak += 1
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
         break
       end
     end
-    
+
     return current_streak
   end
 
