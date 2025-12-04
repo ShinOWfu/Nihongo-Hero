bf1d37d2c5947ca50a326598f84818f29bc1480e
Oliver Will <o.will@alumni.maastrichtuniversity.nl>
2025-12-04 14:26:51 +0900

    Added RWA. Hope it works

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,30 @@
+// app/assets/config/manifest.js
+
+// Link main files
+//= link application.js
+//= link application.css
+
+// Link all files inside the images folder and its subdirectories.
 //= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
-//= link popper.js
-//= link bootstrap.min.js
+
+// Link the PWA manifest
+//= link manifest.json 
+
+// Existing controller link
+//= link controllers/answer_feedback_controller.js 
+
+//= link controllers/application.js
+
+//= link controllers/battle_journal_controller.js
+
+//= link controllers/completed_level_controller.js
+
+//= link controllers/dropdown_controller.js
+//= link controllers/exp_bar_controller.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
+//= link controllers/map_scroll_controller.js
+//= link controllers/mystery_bubble_controller.js
+//= link controllers/results_controller.js
+//= link controllers/scroll_to_current_level_controller.js
+//= link controllers/users_controller.js
